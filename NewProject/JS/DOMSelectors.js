243cbf0b26c234fd98fe6044fb3f9e8e9ef05da2
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2024-11-04 12:29:31 -0500

    e

--- NewProject/JS/DOMSelectors.js
+++ NewProject/JS/DOMSelectors.js
@@ -1,8 +1,3 @@
-const DomSelectors = {
-  personaList: document.getElementById("persona-list"), // The container for the list of personas
-  personaItem: (index) => document.getElementById(`persona-item-${index}`), // Individual persona items
-  personaName: (index) => document.getElementById(`persona-name-${index}`), // Persona name
-  personaLevel: (index) => document.getElementById(`persona-level-${index}`), // Persona level
-  personaArcana: (index) => document.getElementById(`persona-arcana-${index}`), // Persona arcana
-  personaImage: (index) => document.getElementById(`persona-image-${index}`), // Persona image
+const DOMSelectors = {
+  personaList: document.getElementById("selection"),
 };
--- NewProject/JS/main.js
+++ NewProject/JS/main.js
@@ -4,13 +4,16 @@ import { DOMSelectors } from "./DOMSelectors";
 
 function addCard() {
   Personas.forEach((persona, index) =>
-    insertAdjacentHTML(
+    DOMSelectors.selection.insertAdjacentHTML(
       "beforeend",
-      `<div class="card" id="persona-item-${personaItem}>
-      <h1>${DOMSelectors.personaName}</h1>
-      <p>${DOMSelectors.personalevel}</p>
-      <p>${DOMSelectors.personaArcana}</p>
-        </div>`
+      `<div class="card" id="persona-item-${persona.name}>
+      <h1>${persona.name}</h1>
+      <p>${persona.level}</p>
+      <p>${persona.arcana}}</p>
+      <img id="persona-image-${persona.name}" src="${persona.image}" alt="Skibidi" />
+      </div>`
     )
   );
 }
+
+addCard();
