919fb98b68e0b650c5577f0102e12e2e2a7d9752
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2024-11-07 17:03:37 -0500

    e

--- NewProject/JS/main.js
+++ NewProject/JS/main.js
@@ -6,7 +6,7 @@ Personas.forEach((persona) =>
   DOMSelectors.personaList.insertAdjacentHTML(
     "beforeend",
     `<div class="card" id="persona-item-${persona.name}">
-    <div class="arcana" <p>${persona.arcana}</p> </div>
+    <div class="arcana"> <p>${persona.arcana}</p> </div>
     <div class="level"> <p>${persona.level}</p> </div>
     <div class="name"> <p>${persona.name}</p> </div>
     <div class="price"> <p>${persona.price}</p> </div>
