ac3b0d7d0c0a5e8243fba378bc210f728c1a2018
NotLeo27 <158211664+NotLeo27@users.noreply.github.com>
2024-11-08 16:26:38 -0500

    e

--- NewProject/JS/DOMSelectors.js
+++ NewProject/JS/DOMSelectors.js
@@ -3,3 +3,4 @@ const DOMSelectors = {
 };
 
 export { DOMSelectors };
+
--- NewProject/JS/main.js
+++ NewProject/JS/main.js
@@ -1,19 +1,28 @@
 import "../CSS/style.css";
+import "../CSS/variables.css";
 import { Personas } from "./products";
 import { DOMSelectors } from "./DOMSelectors";
 
-Personas.forEach((persona) =>
-  DOMSelectors.personaList.insertAdjacentHTML(
-    "beforeend",
-    `<div class="card" id="persona-item-${persona.name}">
-    <div class="arcana"> <p>${persona.arcana}</p> </div>
-    <div class="level"> <p>${persona.level}</p> </div>
-    <div class="name"> <p>${persona.name}</p> </div>
-    <div class="price"> <p>${persona.price}</p> </div>
-    
-    </div>`
-  )
-);
+function addCards() {
+  Personas.forEach((persona) =>
+    DOMSelectors.personaList.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card" id="persona-item-${persona.name}">
+      <div class="arcana"> <p>${persona.arcana}</p> </div>
+      <div class="level"> <p>${persona.level}</p> </div>
+      <div class="name"> <p>${persona.name}</p> </div>
+      <div class="price"> <p>${persona.price}</p> </div>
+      
+      </div>`
+    )
+  );
+}
+
+function clear() {
+
+}
+
+addCards()
 
 /* 
 
